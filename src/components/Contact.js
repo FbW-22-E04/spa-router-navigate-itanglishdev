90e7d9d92acf44bc1aad10d49eed809a4c590ec1
Chris <chrishethertonweb@gmail.com>
2022-10-29 15:27:18 +0200

    added useNavigate

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
-  return <h1>Contact</h1>;
+ const navigation = useNavigate()
+
+ const goBack =()=> {
+  navigation('/home')
+ }
+
+  return <div>
+    <h1>Contact</h1>
+    <button onClick={goBack} >Submit</button>
+  </div>;
 };
 
 export default Contact;
